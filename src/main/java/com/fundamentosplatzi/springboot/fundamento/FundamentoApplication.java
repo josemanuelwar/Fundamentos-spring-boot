e963805dd4cc43714d631f3198ec30c6d36c7622
jose manuel <josemanuelwar@hotmail.com>
2023-05-14 15:22:54 -0600

    mi primer commit

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamento/FundamentoApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.fundamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosplatzi/springboot/fundamento/FundamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.fundamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
